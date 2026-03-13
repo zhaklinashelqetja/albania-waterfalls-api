6935ae7029e97eee93445b3af044191535a1df58
zhaklinashelqetja <234790815+zhaklinashelqetja@users.noreply.github.com>
2026-03-13 08:15:01 +0100

    Implement basic auth validation

--- src/routes/api/waterfalls/[id]/+server.js
+++ src/routes/api/waterfalls/[id]/+server.js
@@ -1,4 +1,17 @@
 import pool from '$lib/server/database.js';
+import { API_USER, API_PASS } from '$env/static/private';
+
+function checkAuth(request) {
+    const auth = request.headers.get('Authorization');
+
+    if (!auth?.startsWith('Basic ')) return false;
+
+    const base64 = auth.slice(6);
+    const decoded = atob(base64);
+    const [user, pass] = decoded.split(':');
+
+    return user === API_USER && pass === API_PASS;
+}
 
 export async function GET({ params }) {
 
